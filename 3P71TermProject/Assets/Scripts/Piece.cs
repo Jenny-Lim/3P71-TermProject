903bd80060a327d83cb1c63136a88574e2bf55d3
Jenny-Lim <180jnlm@gmail.com>
2022-12-12 15:27:28 -0500

    made board arr

--- 3P71TermProject/Assets/Scripts/Piece.cs
+++ 3P71TermProject/Assets/Scripts/Piece.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardScript : MonoBehaviour
+public class Piece : MonoBehaviour
 {
 
-    Piece board[][] = new Piece[][];
+    int score;
+    bool isTaken;
+    int xPosition;
+    int yPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        // pogchamp
-        //test line 2 
+        
     }
 
     // Update is called once per frame
